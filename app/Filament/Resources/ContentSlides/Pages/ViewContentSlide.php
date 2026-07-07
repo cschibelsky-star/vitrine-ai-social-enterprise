f50d1690b65a5c697f6d15594c49430badd423b3
cschibelsky-star <cschibelsky@gmail.com>
2026-07-07 14:52:34 -0300

    Adiciona detalhe de slide gerado

---
+++ app/Filament/Resources/ContentSlides/Pages/ViewContentSlide.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ContentSlides\Pages;
+
+use App\Filament\Resources\ContentSlides\ContentSlideResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewContentSlide extends ViewRecord
+{
+    protected static string $resource = ContentSlideResource::class;
+}
