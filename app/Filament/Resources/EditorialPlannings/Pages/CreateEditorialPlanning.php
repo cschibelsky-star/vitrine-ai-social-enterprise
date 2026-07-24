339cfc0a2a8291e6018d18ca4b3fa4a32ecb2909
cschibelsky-star <cschibelsky@gmail.com>
2026-07-24 09:30:36 -0300

    feat: add editorial planning create page

---
+++ app/Filament/Resources/EditorialPlannings/Pages/CreateEditorialPlanning.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\EditorialPlannings\Pages;
+
+use App\Filament\Resources\EditorialPlannings\EditorialPlanningResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateEditorialPlanning extends CreateRecord
+{
+    protected static string $resource = EditorialPlanningResource::class;
+}
