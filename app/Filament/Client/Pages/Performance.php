b83971b2d7ae3b7a7d735ebee9cddeb9408e97c2
cschibelsky-star <cschibelsky@gmail.com>
2026-09-13 21:10:43 -0300

    feat(client): add performance page

---
+++ app/Filament/Client/Pages/Performance.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Client\Pages;
+
+class Performance extends BaseClientSection
+{
+    protected static ?string $navigationLabel = 'Desempenho';
+    protected static ?string $title = 'Desempenho';
+    protected static ?int $navigationSort = 30;
+    protected static ?string $slug = 'desempenho';
+    public static string $sectionKey = 'performance';
+}
