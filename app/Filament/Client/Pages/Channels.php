90888c66a614805a4de8d705fac2154be3fb87bb
cschibelsky-star <cschibelsky@gmail.com>
2026-09-13 21:10:48 -0300

    feat(client): add channels page

---
+++ app/Filament/Client/Pages/Channels.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Client\Pages;
+
+class Channels extends BaseClientSection
+{
+    protected static ?string $navigationLabel = 'Canais';
+    protected static ?string $title = 'Canais';
+    protected static ?int $navigationSort = 40;
+    protected static ?string $slug = 'canais';
+    public static string $sectionKey = 'channels';
+}
