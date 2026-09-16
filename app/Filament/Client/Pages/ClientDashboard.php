fda0ec6072d3e370b333645b66c2ab9f7081d205
cschibelsky-star <cschibelsky@gmail.com>
2026-09-16 18:36:22 -0300

    fix(client): materialize approved dashboard widgets

--- app/Filament/Client/Pages/ClientDashboard.php
+++ app/Filament/Client/Pages/ClientDashboard.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Client\Pages;
 
+use App\Filament\Client\Widgets\ClientBalanceOverview;
+use App\Filament\Client\Widgets\ClientCommandCenter;
 use Filament\Pages\Dashboard;
 
 class ClientDashboard extends Dashboard
@@ -11,4 +13,17 @@ class ClientDashboard extends Dashboard
     protected static ?string $title = 'Painel';
 
     protected static ?int $navigationSort = -10;
+
+    public function getWidgets(): array
+    {
+        return [
+            ClientCommandCenter::class,
+            ClientBalanceOverview::class,
+        ];
+    }
+
+    public function getColumns(): int | string | array
+    {
+        return 1;
+    }
 }
