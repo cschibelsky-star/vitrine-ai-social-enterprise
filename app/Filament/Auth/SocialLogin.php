4cdf75534a4ed28e2a17839f81de6c47e9a32dc4
cschibelsky-star <cschibelsky@gmail.com>
2026-09-16 18:36:48 -0300

    fix(auth): localize and repair client password field

--- app/Filament/Auth/SocialLogin.php
+++ app/Filament/Auth/SocialLogin.php
@@ -3,8 +3,39 @@
 namespace App\Filament\Auth;
 
 use Filament\Auth\Pages\Login;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Component;
 
 class SocialLogin extends Login
 {
     protected string $view = 'filament.auth.social-login';
+
+    protected function getEmailFormComponent(): Component
+    {
+        return TextInput::make('email')
+            ->label('E-mail')
+            ->email()
+            ->required()
+            ->autocomplete('email')
+            ->autofocus()
+            ->extraInputAttributes(['tabindex' => 1]);
+    }
+
+    protected function getPasswordFormComponent(): Component
+    {
+        return TextInput::make('password')
+            ->label('Senha')
+            ->password()
+            ->revealable()
+            ->required()
+            ->autocomplete('current-password')
+            ->extraInputAttributes(['tabindex' => 2]);
+    }
+
+    protected function getRememberFormComponent(): Component
+    {
+        return Checkbox::make('remember')
+            ->label('Lembrar acesso');
+    }
 }
