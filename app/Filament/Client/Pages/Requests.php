b9c22955b1a07132eaf5988faf3a1f9dd78223b3
cschibelsky-star <cschibelsky@gmail.com>
2026-09-16 09:41:19 -0300

    feat(client): add requests page

---
+++ app/Filament/Client/Pages/Requests.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Client\Pages;
+
+class Requests extends BaseClientSection
+{
+    protected static ?string $navigationLabel = 'Solicitações';
+    protected static ?string $title = 'Solicitações';
+    protected static ?int $navigationSort = 50;
+    protected static ?string $slug = 'solicitacoes';
+    public static string $sectionKey = 'requests';
+}
