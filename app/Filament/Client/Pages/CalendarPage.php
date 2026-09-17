394399081e8713d654abb13d58b30a31c1b442e2
cschibelsky-star <cschibelsky@gmail.com>
2026-09-17 20:53:33 -0300

    fix(client): restore calendar labels

--- app/Filament/Client/Pages/CalendarPage.php
+++ app/Filament/Client/Pages/CalendarPage.php
@@ -4,8 +4,8 @@ namespace App\Filament\Client\Pages;
 
 class CalendarPage extends BaseClientSection
 {
-    protected static ?string $navigationLabel = 'Calendario';
-    protected static ?string $title = 'Calendario Editorial';
+    protected static ?string $navigationLabel = 'Calendário';
+    protected static ?string $title = 'Calendário Editorial';
     protected static ?int $navigationSort = 20;
     protected static ?string $slug = 'calendario';
     public static string $sectionKey = 'calendar';
